6a9209218d946e21c96714f4dc28dcf4ae10a514
aq2r <183514119+aq2r@users.noreply.github.com>
2024-11-30 15:19:45 +0900

    cargo fix

--- src/lang_yaml.rs
+++ src/lang_yaml.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, ops::Deref};
 
-use crate::{YAML_MODIFIED_TIME, YAML_PATH};
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(transparent)]
 pub(crate) struct LangYaml(pub HashMap<String, LocalizedText>);
